9a2eda6ffce33657d90ab66083ed513067367866
pc20211065 <115679981+pc20211065@users.noreply.github.com>
2022-10-20 10:36:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,47 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(1, 6))
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    count = -1
 })
